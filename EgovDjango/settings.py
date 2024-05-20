aa41061c393e59b29bc43d098962c260e8a05862
DarkhanMami <dmami@nu.edu.kz>
2024-05-20 12:13:47 +0000

    server config

--- EgovDjango/settings.py
+++ EgovDjango/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/webapps/egov/Egov/static/'
+STATIC_ROOT = '/webapps/egov/Egov/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
